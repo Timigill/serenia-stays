92a4cdeae9711a348f9b7c9c48ace5614ce249ab
Tanzeela Memon <tanzeelamemon.bscsf22@iba-suk.edu.pk>
2025-07-15 15:54:07 +0500

    added about section in home page with left side text and right side images , as saw in sample design

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import HeroSection from '../components/home/HeroSection';
+import AboutSection from "../components/home/AboutSection";
+
 
 export default function Page() {
   return (
     <>
       <HeroSection />
+      <AboutSection />
       
     </>
   );
